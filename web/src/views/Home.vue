29e646e6acc38dd124917bfe1f6f5d9d09342b5d
lebrancconvas <lebrancconvas@gmail.com>
2021-12-14 01:58:37 +0700

    :tada: Re-Init.

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,17 +1,18 @@
 <template>
-  <div>
-    <HelloWorld/>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import HelloWorld from '../components/HelloWorld.vue';
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  
   components: {
-    HelloWorld,
-  },
-};
+    HelloWorld
+  }
+}
 </script>
--- web/src/views/Testpage.vue
+++
@@ -1,5 +0,0 @@
-<template>
-	<div>
-		<h1>Template Test.</h1> 
-	</div>
-</template>
