af5f2ce2169ffce4143d3dcbf2a0b79246450d7c
lebrancconvas <lebrancconvas@gmail.com>
2021-12-14 00:59:58 +0700

    ➕ Add Vuetify 3 Preview.

--- web/src/App.vue
+++ web/src/App.vue
@@ -1,7 +1,18 @@
 <template>
-  <router-view/>
+  <v-app>
+    <v-main>
+      <router-view/>
+    </v-main>
+  </v-app>
 </template>
 
-<style>
+<script>
 
-</style> 
+export default {
+  name: 'App',
+
+  data: () => ({
+    //
+  }),
+}
+</script>
