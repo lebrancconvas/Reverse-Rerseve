b65042442cb704e3eb6e396254f7c4445c726602
lebrancconvas <lebrancconvas@gmail.com>
2021-12-14 18:32:57 +0700

    🚚 Re-Construction Project.

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h1>Reverse - Rerseve</h1>
+    <input type="text" name="wordInput" id="wordInput"> 
+    <button>
+      Submit 
+    </button>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
 }
 </script>
